42b0b936c395cd9eede646039fd25d8e85676284
Kassim Sheghembe <sheghembekassim@gmail.com>
2024-06-05 12:44:50 +0300

    Create ADDO Visit visit and event per client type.

--- app/src/main/java/org/smartregister/addo/interactor/AddoVisitInteractor.java
+++ app/src/main/java/org/smartregister/addo/interactor/AddoVisitInteractor.java
@@ -1,8 +1,9 @@
 package org.smartregister.addo.interactor;
 
-import org.smartregister.addo.dao.AncDao;
 import org.smartregister.addo.dao.VisitDao;
+import org.smartregister.addo.util.Constants;
 import org.smartregister.addo.util.Constants.FamilyMemberType;
+import org.smartregister.addo.util.CoreConstants;
 import org.smartregister.chw.anc.contract.BaseAncHomeVisitContract;
 import org.smartregister.chw.anc.domain.MemberObject;
 import org.smartregister.chw.anc.interactor.BaseAncHomeVisitInteractor;
@@ -72,6 +73,42 @@ public class AddoVisitInteractor extends BaseAncHomeVisitInteractor {
         };
 
         appExecutors.diskIO().execute(runnable);
+    }
+
+    @Override
+    protected String getEncounterType() {
+        String encounterType;
+        if (clientType.equals(FamilyMemberType.CHILD)) {
+            encounterType = CoreConstants.EventType.CHILD_ADDO_VISIT;
+        } else if (clientType.equals(FamilyMemberType.ANC)) {
+            encounterType = CoreConstants.EventType.ANC_ADDO_VISIT;
+        } else if (clientType.equals(FamilyMemberType.PNC)) {
+            encounterType = CoreConstants.EventType.PNC_ADDO_VISIT;
+        } else if (clientType.equals(FamilyMemberType.ADOLESCENT)) {
+            encounterType = CoreConstants.EventType.ADOLESCENT_ADDO_VISIT;
+        } else {
+            encounterType = "Home Visit";
+        }
+
+        return encounterType;
+    }
+
+    @Override
+    protected String getTableName() {
+        String tableName;
+        if (clientType.equals(FamilyMemberType.CHILD)) {
+            tableName = Constants.TABLE_NAME.CHILD;
+        } else if (clientType.equals(FamilyMemberType.ANC)) {
+            tableName = Constants.TABLE_NAME.ANC_MEMBER;
+        } else if (clientType.equals(FamilyMemberType.PNC)) {
+            tableName = Constants.TABLE_NAME.ANC_PREGNANCY_OUTCOME;
+        } else if (clientType.equals(FamilyMemberType.ADOLESCENT)) {
+            tableName = Constants.TABLE_NAME.ADOLESCENT;
+        } else {
+            tableName = "ec_family_member";
+        }
+
+        return tableName;
     }
 
     public interface Flavor {

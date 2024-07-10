d47ff04ecb43d73c3655f70cd5d12fba054e49a1
issyzac <issyzac.iz@gmail.com>
2024-07-10 15:53:41 +0300

    :recycle: fix broken query

--- app/src/main/java/org/smartregister/addo/repository/AddoWeeklySummaryRepository.java
+++ app/src/main/java/org/smartregister/addo/repository/AddoWeeklySummaryRepository.java
@@ -114,7 +114,7 @@ public class AddoWeeklySummaryRepository {
                     "'Other Member ADDO Visit', " +
                     "'Adolescent ADDO Visit') and " +
                     "date(datetime(visit_date/1000, 'unixepoch')) > datetime('now', 'start of day','-6 days') and " +
-                    BASE_ENTITY_ID + " IN(select \"for\" from task where priority = '2' and " +
+                    BASE_ENTITY_ID + " IN(select \"for\" from task where code = 'Linkage' and " +
                     "date(datetime(start/1000, 'unixepoch')) > datetime('now', 'start of day', '-6 days') and " +
                     "status IN ('COMPLETED', 'IN_PROGRESS')) group by base_entity_id, visit_date;";
             cursor = repository.getReadableDatabase().rawQuery(query, null);
